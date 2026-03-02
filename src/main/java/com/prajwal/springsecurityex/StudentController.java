41886aed7479b4ce300e6748c89099da873cd16f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-02 11:36:42 -0600

    Hard coded the list of students in the student controller to check if it works

--- src/main/java/com/prajwal/springsecurityex/StudentController.java
+++ src/main/java/com/prajwal/springsecurityex/StudentController.java
@@ -1,8 +1,23 @@
 package com.prajwal.springsecurityex;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
+
+    private List<Student> students = new ArrayList<>(List.of(
+            new Student(1, "Prajwal", 58),
+            new Student(2, "Akkt", 97),
+            new Student(3, "Joseph", 90)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return students;
+    }
 
 }
