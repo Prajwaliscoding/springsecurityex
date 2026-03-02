25038acbab5b22438d46268a9df6359b3cf8575f
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-02 11:31:18 -0600

    Getting the CSRF token

---
+++ src/main/java/com/prajwal/springsecurityex/Student.java
@@ -0,0 +1,46 @@
+package com.prajwal.springsecurityex;
+
+public class Student {
+    private int id;
+    private String name;
+    private int mark;
+
+    public Student(int mark, String name, int id) {
+        this.mark = mark;
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", mark=" + mark +
+                '}';
+    }
+}
