62105003fedfb43d787249a5e107ef9ef28f3552
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-03 15:54:42 -0600

    Added the logic for getting the CSRF token from the client side/browser

--- src/main/java/com/prajwal/springsecurityex/StudentController.java
+++ src/main/java/com/prajwal/springsecurityex/StudentController.java
@@ -1,6 +1,11 @@
 package com.prajwal.springsecurityex;
 
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,7 +14,7 @@ import java.util.List;
 @RestController
 public class StudentController {
 
-    private List<Student> students = new ArrayList<>(List.of(
+    private final List<Student> students = new ArrayList<>(List.of(
             new Student(1, "Prajwal", 58),
             new Student(2, "Akkt", 97),
             new Student(3, "Joseph", 90)
@@ -18,6 +23,17 @@ public class StudentController {
     @GetMapping("/students")
     public List<Student> getStudents(){
         return students;
+    }
+
+    @PostMapping("/students")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+
+    @GetMapping("/csrf-tokem")
+    public CsrfToken getCsrf(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
     }
 
 }
