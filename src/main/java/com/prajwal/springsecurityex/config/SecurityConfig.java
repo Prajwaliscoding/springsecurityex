ba5d17acd96be8751f10afc8f0113a05c599fc0e
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-04 23:46:11 -0600

    Added annotation to make it follow our flow and not the default ones

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.prajwal.springsecurityex.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 }
