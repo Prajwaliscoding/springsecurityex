c07b0e99f7aa9de379657a5afef5ee0d6c5274f1
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-05 23:51:01 -0600

    Added bean

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.prajwal.springsecurityex.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
 
 }
