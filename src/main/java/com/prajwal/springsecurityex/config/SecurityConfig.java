bc554d5775b8578a5e070299f960401b255ea27e
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-05 23:52:14 -0600

    Added new function for filter chain

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -3,10 +3,14 @@ package com.prajwal.springsecurityex.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    public SecurityFilterChain securityFilterChain(){
+        return 
+    }
 
 }
