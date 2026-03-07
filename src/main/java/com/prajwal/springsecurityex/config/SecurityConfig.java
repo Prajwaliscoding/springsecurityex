c1fe2cecdffd3f974e29ece9be4bca94795039ca
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-06 23:51:20 -0600

    Made it suitatble for Postmann

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.prajwal.springsecurityex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +15,8 @@ public class SecurityConfig {
 
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-        
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
