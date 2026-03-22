b9916033cb5b067dfa3438c90e980cdf5cf5ff46
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-22 17:48:05 -0500

    After disabling CSRF, added authorization and login for both browser and postman, for both kinda client

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.prajwal.springsecurityex.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,11 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
+        http.csrf(customizer -> customizer.disable());
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
 }
