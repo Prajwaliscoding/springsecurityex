9e94339384bc118fe1e2659ad075b700d4465593
Prajwaliscoding <prajwalkhatiwada.formal@gmail.com>
2026-03-22 19:15:52 -0500

    Re-written using build-up method

--- src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
+++ src/main/java/com/prajwal/springsecurityex/config/SecurityConfig.java
@@ -14,12 +14,14 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
-        http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
-//        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        return http.build();
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .build();
     }
 }
